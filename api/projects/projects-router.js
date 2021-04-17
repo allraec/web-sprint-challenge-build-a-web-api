37506a4b459c8992ce6121dbda9507c750947317
Allison Castaneda <allraec@yahoo.com>
2021-04-16 22:18:51 -0500

    GET req ready

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,22 @@
-// Write your "projects" router here!
+const express = require("express");
+const db =  require("./projects-model");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+    db.get()
+        .then(projects => {
+            res.json(projects);
+        })
+        .catch(next)
+});
+
+router.get("/:id", (req, res, next) => {
+    db.get(req.params.id)
+        .then(project => {
+            res.json(project);
+        })
+        .catch(next)
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,22 @@
 const express = require('express');
+const projectsRouter = require("./projects/projects-router");
+const actionsRouter = require("./actions/actions-router");
+
 const server = express();
 
-// Complete your server here!
-// Do NOT `server.listen()` inside this file!
+server.get("/", (req, res) => {
+    res.send(`<h1>API Available</h1>`);
+});
+
+server.use(express.json());
+server.use("/api/projects/", projectsRouter);
+//server.use(actionsRouter);
+
+server.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({
+        message: "Something went wrong"
+    })
+})
 
 module.exports = server;
