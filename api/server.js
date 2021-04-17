12e29d238dd6d0e38373dcaf20564f3fc4a6672e
Allison Castaneda <allraec@yahoo.com>
2021-04-16 23:12:41 -0500

    middleware project post req

--- api/server.js
+++ api/server.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const projectsRouter = require("./projects/projects-router");
 const actionsRouter = require("./actions/actions-router");
-
+const { logger } = require("./middleware/middleware");
 const server = express();
+
+server.use(logger())
 
 server.get("/", (req, res) => {
     res.send(`<h1>API Available</h1>`);
